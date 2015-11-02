0e406ee1645016d45dc77becc799854ce0d050ed
LovinSpoonful <robhodde@yahoo.com>
2015-11-02 18:58:20 -0500

    Update Week1 - Quiz.R
    Added additional notes to explain the solution (since it is almost verbatim the source link.)

--- Week1 - Quiz.R
+++ Week1 - Quiz.R
@@ -19,7 +19,16 @@ print(a)
 
 
 # 3. Show how to take a trio of input numbers a, b, and c and implement the quadratic equation.
+
 # From: https://rstudio-pubs-static.s3.amazonaws.com/46122_3840087773cd4f5a8629ef0228ab152a.html
+# We are solving for x = 0, or "the quadratic must be zero":
+
+# -b +- sqrt(b^2 - 4ac)
+# ----------------------  = 0
+#         2a
+
+# this formula has two possible solutions due to the +- sign preceding the square root calculation
+
 
 a <- 1
 b <- -7
